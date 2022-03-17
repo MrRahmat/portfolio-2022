1a9ba4e08e067c27d0f8709393697e8e9e55b107
MrRahmat <vovacet12@mail.ru>
2022-03-17 13:47:36 +0300

    Add img for About

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ At last I decided to create my portfolio.
 12.03 - Still don't know what I want to get in the end. Playing with fonts and animations.
 
 15.03 - Playing with images and animations. Complete the intro component
+16.03 - Configured Jenkink for AWS. Now app is avaidable via https://vshell.xyz
+17.03 - Working with About section
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,19 @@
 import './About.modules.css';
+import Me from '../../img/card.png';
 
 function About() {
   return (
-    <div className='about'>About</div>
+    <div className='about'>
+      <div className="about-left">
+        <div className="card bg"></div>
+        <div className="card">
+          <img src={Me} alt="card"/>
+        </div>
+      </div>
+      <div className="about-right">
+        Right
+      </div>
+    </div>
   )
 }
 
