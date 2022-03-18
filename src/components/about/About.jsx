188f1a468ba675f904714f0a6687944d9c45dc19
MrRahmat <vovacet12@mail.ru>
2022-03-18 23:52:34 +0300

    More detail for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,7 @@
 import './About.modules.css';
 import Me from '../../img/card.png';
+import MeToo from '../../img/card1.png';
+import Me1 from '../../img/Frame1.png';
 
 function About() {
   return (
@@ -7,13 +9,46 @@ function About() {
       <div className="about-left">
         <div className="card bg"></div>
         <div className="card">
-          <img src={Me} alt="card"/>
+          <img src={Me1} alt="card"/>
         </div>
       </div>
       <div className="about-right">
-        Right
+        <div className="about-wrapper">
+          <h1 className='a-h1'>DevOps</h1>
+          <div className="about-item">
+            <h3 className='about-item-title'>Ansibel + Terraform</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aliquid provident aliquam vitae odio fugiat amet ratione unde qui, illum officia vero fugit! Ducimus labore, illum perspiciatis eligendi tenetur cumque recusandae ut pariatur voluptas nesciunt, tempora vel. Maiores, nobis officia!
+            </p>
+          </div>
+          <div className="about-item">
+            <h3 className='about-item-title'>Ansibel + Terraform</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aliquid provident aliquam vitae odio fugiat amet ratione unde qui, illum officia vero fugit! Ducimus labore, illum perspiciatis eligendi tenetur cumque recusandae ut pariatur voluptas nesciunt, tempora vel. Maiores, nobis officia!
+            </p>
+          </div>
+          <div className="about-item">
+            <h3 className='about-item-title'>Ansibel + Terraform</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aliquid provident aliquam vitae odio fugiat amet ratione unde qui, illum officia vero fugit! Ducimus labore, illum perspiciatis eligendi tenetur cumque recusandae ut pariatur voluptas nesciunt, tempora vel. Maiores, nobis officia!
+            </p>
+          </div>
+          <div className="about-item">
+            <h3 className='about-item-title'>Ansibel + Terraform</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aliquid provident aliquam vitae odio fugiat amet ratione unde qui, illum officia vero fugit! Ducimus labore, illum perspiciatis eligendi tenetur cumque recusandae ut pariatur voluptas nesciunt, tempora vel. Maiores, nobis officia!
+            </p>
+          </div>
+          <div className="about-item">
+            <h3 className='about-item-title'>Ansibel + Terraform</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aliquid provident aliquam vitae odio fugiat amet ratione unde qui, illum officia vero fugit! Ducimus labore, illum perspiciatis eligendi tenetur cumque recusandae ut pariatur voluptas nesciunt, tempora vel. Maiores, nobis officia!
+            </p>
+          </div>
+          
+        </div>
       </div>
-    </div>
+    </div>  
   )
 }
 
